9575314374bc83955fc0e05c837bc0ad6d3fd75c
seon <sunkyoung.dev@gmail.com>
2023-08-11 17:38:59 +0900

    ..

--- 백준/2563.py
+++ 백준/2563.py
@@ -1,33 +1,18 @@
 #  **************************************************************************  #
 #                                                                              #
 #                                                       :::    :::    :::      #
-#    Problem Number: 10798                             :+:    :+:      :+:     #
+#    Problem Number: 2563                              :+:    :+:      :+:     #
 #                                                     +:+    +:+        +:+    #
 #    By: jsk2342 <boj.kr/u/jsk2342>                  +#+    +#+          +#+   #
 #                                                   +#+      +#+        +#+    #
-#    https://boj.kr/10798                          #+#        #+#      #+#     #
-#    Solved: 2023/08/10 15:33:29 by jsk2342       ###          ###   ##.kr     #
+#    https://boj.kr/2563                           #+#        #+#      #+#     #
+#    Solved: 2023/08/11 15:23:27 by jsk2342       ###          ###   ##.kr     #
 #                                                                              #
 #  **************************************************************************  #
 
+'''
+색종이는 무조건 길이 10cm 정사각형
+count=색종이수
+x,y=색종이가 시작되는 x,y좌표
 
-word_list=[[0]*1 for _ in range(5)]
-max_len=0
-sentence=''
-
-for i in range(5):
-    word=list(input())
-    word_list[i]=word
-    if(max_len<len(word)):
-        max_len=len(word)
-
-for c in range(max_len):
-    for r in range(5):
-        try:
-            sentence+=word_list[r][c]
-        except:
-            sentence+=''
-print(sentence)         
-
-
-
+'''
